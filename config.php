f018dd56508eeee71b5123558b367182d585e755
Danilo <daniloaugusto0212@hotmail.com>
2021-04-13 19:07:04 -0300

    Update

--- config.php
+++ config.php
@@ -1,66 +1,69 @@
 <?php
 
-	session_start();
-	date_default_timezone_set('America/Sao_Paulo');
-	$autoload = function($class){
-		if($class == 'Email'){
+session_start();
+date_default_timezone_set('America/Sao_Paulo');
+$autoload = function ($class) {
+    if ($class == 'Email') {
+        require_once('classes/phpmailer/PHPMailerAutoload.php');
+    }
+    include('classes/' . $class . '.php');
+};
 
-			require_once('classes/phpmailer/PHPMailerAutoload.php');
-		}
-		include('classes/'.$class.'.php');
-	};
-
-	spl_autoload_register($autoload);
-
-
-	define('INCLUDE_PATH','http://localhost/rede_social/');
-	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
-
-	define('BASE_DIR_PAINEL',__DIR__.'/painel');
+spl_autoload_register($autoload);
 
 
-	//Conectar com banco de dados!
-	define('HOST','localhost');
-	define('USER','root');
-	define('PASSWORD','');
-	define('DATABASE','sistema');
+define('INCLUDE_PATH', 'http://localhost/rede_social/');
+define('INCLUDE_PATH_PAINEL', INCLUDE_PATH . 'painel/');
 
-	//Constantes para o painel de controle
-	define('NOME_EMPRESA','Danki Code');
+define('BASE_DIR_PAINEL', __DIR__ . '/painel');
 
-	//Funções do painel
-	function pegaCargo($indice){
-		return Painel::$cargos[$indice];
-	}
 
-	function selecionadoMenu($par){
-		/*<i class="fa fa-angle-double-right" aria-hidden="true"></i>*/
-		$url = explode('/',@$_GET['url'])[0];
-		if($url == $par){
-			echo 'class="menu-active"';
-		}
-	}
+//Conectar com banco de dados!
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'sistema');
 
-	function verificaPermissaoMenu($permissao){
-		if($_SESSION['cargo'] >= $permissao){
-			return;
-		}else{
-			echo 'style="display:none;"';
-		}
-	}
+//Constantes para o painel de controle
+define('NOME_EMPRESA', 'Danilo');
 
-	function verificaPermissaoPagina($permissao){
-		if($_SESSION['cargo'] >= $permissao){
-			return;
-		}else{
-			include('painel/pages/permissao_negada.php');
-			die();
-		}
-	}
+//Funções do painel
+function pegaCargo($indice)
+{
+    return Painel::$cargos[$indice];
+}
 
-	function recoverPost($post){
-		if(isset($_POST[$post])){
-			echo $_POST[$post];
-		}
-	}
-?>
+function selecionadoMenu($par)
+{
+    /*<i class="fa fa-angle-double-right" aria-hidden="true"></i>*/
+    $url = explode('/', @$_GET['url'])[0];
+    if ($url == $par) {
+        echo 'class="menu-active"';
+    }
+}
+
+function verificaPermissaoMenu($permissao)
+{
+    if ($_SESSION['cargo'] >= $permissao) {
+        return;
+    } else {
+        echo 'style="display:none;"';
+    }
+}
+
+function verificaPermissaoPagina($permissao)
+{
+    if ($_SESSION['cargo'] >= $permissao) {
+        return;
+    } else {
+        include('painel/pages/permissao_negada.php');
+        die();
+    }
+}
+
+function recoverPost($post)
+{
+    if (isset($_POST[$post])) {
+        echo $_POST[$post];
+    }
+}
